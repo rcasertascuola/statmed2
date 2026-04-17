0e63d7b60b9d9a330896dfb183b46acfa6e5543c
rcasertascuola <raffaello.caserta@majoranapa.edu.it>
2026-04-17 14:32:20 +0200

    Change database connection from SQLite to MySQL

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_PASS', '');
 
 function getDB() {
     try {
-        $pdo = new PDO("sqlite:database_ocr.sqlite");
+        $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $pdo;
